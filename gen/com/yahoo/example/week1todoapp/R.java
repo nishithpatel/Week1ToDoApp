6ea1bd5fea4831f6e31c76ef10d0e381c839af8b
Nishith Patel <nishithpatel@yahoo.com>
2014-01-14 16:37:47 -0800

    version 1.0
    Completed

--- gen/com/yahoo/example/week1todoapp/R.java
+++ gen/com/yahoo/example/week1todoapp/R.java
@@ -24,11 +24,10 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080004;
-        public static final int addButton=0x7f080003;
-        public static final int addItem=0x7f080001;
-        public static final int editList=0x7f080000;
-        public static final int listView1=0x7f080002;
+        public static final int action_settings=0x7f080003;
+        public static final int btnAddItem=0x7f080001;
+        public static final int etNewItem=0x7f080002;
+        public static final int lvToDoList=0x7f080000;
     }
     public static final class layout {
         public static final int activity_todo=0x7f030000;
@@ -41,6 +40,7 @@ public final class R {
         public static final int add_label=0x7f050004;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050002;
+        public static final int newItemHint=0x7f050005;
         public static final int test=0x7f050003;
     }
     public static final class style {
--- src/com/yahoo/example/week1todoapp/TodoActivity.java
+++ src/com/yahoo/example/week1todoapp/TodoActivity.java
@@ -1,22 +1,85 @@
 package com.yahoo.example.week1todoapp;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.apache.commons.io.FileUtils;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemLongClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Toast;
 
 public class TodoActivity extends Activity {
+	
+	private ArrayList<String> todoItems;
+	private ArrayAdapter<String> todoAdapter;
+	private ListView lvToDoList;
+	private EditText etNewItem;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_todo);
+		etNewItem = (EditText) findViewById(R.id.etNewItem);
+		lvToDoList = (ListView) findViewById(R.id.lvToDoList);
+		readItems();
+		todoAdapter = new ArrayAdapter<String>(getBaseContext(), android.R.layout.simple_list_item_1, todoItems);
+		lvToDoList.setAdapter(todoAdapter);
+		setupListViewListener();
 	}
 
-	public void onAddItem(View v) {
+	private void setupListViewListener() {
 		// TODO Auto-generated method stub
-		Toast.makeText(getBaseContext(), "Hello World", Toast.LENGTH_SHORT).show();
+		lvToDoList.setOnItemLongClickListener(new OnItemLongClickListener() {
+
+			@Override
+			public boolean onItemLongClick(AdapterView<?> adapter, View item, int pos, long id) {
+				todoItems.remove(pos);
+				todoAdapter.notifyDataSetChanged();
+				writeItems();
+				return false;
+			}
+			
+		});
+	}
+	
+	public void addNewItem(View v) {
+		// TODO Auto-generated method stub
+		String newItemText = etNewItem.getText().toString();
+		todoAdapter.add(newItemText);
+		etNewItem.setText("");
+		writeItems();
+	}
+	
+	private void readItems() {
+		
+		File fileDir = getFilesDir();
+		File todoFile = new File(fileDir, "todo.txt");
+		try {
+			todoItems = new ArrayList<String>(FileUtils.readLines(todoFile));
+		} catch (IOException io) {
+			todoItems = new ArrayList<String>();
+		}
+	}
+	
+	private void writeItems() {
+		
+		File fileDir = getFilesDir();
+		File todoFile = new File(fileDir, "todo.txt");
+		try {
+			FileUtils.writeLines(todoFile, todoItems);
+		} catch (IOException io) {
+			io.printStackTrace();
+		}
+
 	}
 
 }
